1a1a9dbb3d7a3fd5ae7444a5188f14413be91b87
MashkinA <cool.solt12@ya.ru>
2025-04-16 20:00:44 +0300

    ed

--- 1_Bored app/code.js
+++ 1_Bored app/code.js
@@ -1,16 +1,40 @@
 const btnNode = document.getElementById('btn');
+const h1Node = document.getElementById('h1');
+const h2Node = document.getElementById('h2');
+const stickerNode = document.getElementById('sticker');
+
+let q = '';
+
+API ();
+
+
+
 
 btnNode.addEventListener('click', function() {
-    fetch("https://fakestoreapi.com/products/1")
-    .then((res) => {
-        if (res.status !== 'success') {
-            return;
-        }
-        const act = res.title;
-        document.getElementById('html_msg').innerHTML = ``;
-        console.log(act);
-    });
+    changeH1();
+    changeH2(q);
+    changeSticker();
 });
 
 
 
+function API() {
+    fetch('https://dummyjson.com/todos/random')
+    .then(data => data.json())
+    .then((res) => {
+        //if (res.status !== 'success') {
+        //    return;
+        //}
+        q = res.todo;
+        console.log(typeof(q));
+    });
+};
+function changeH1 () {
+    h1Node.innerText = 'Now go and try:'
+};
+function changeH2 (task) {
+    h2Node.innerText = `${task}`;
+;}
+function changeSticker() {
+    stickerNode.classList.add('sticker_comp');
+}
