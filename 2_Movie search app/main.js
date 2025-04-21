2f1324e74fb24ebeb6a9535c21511b50f859ee7e
MashkinA <cool.solt12@ya.ru>
2025-04-21 23:03:26 +0300

    ed

--- 2_Movie search app/main.js
+++ 2_Movie search app/main.js
@@ -4,18 +4,22 @@ const inputNode = document.getElementById('input');
 const btnNode = document.getElementById('inputBtn');
 const listNode = document.getElementById('list');
 const popUpNode = document.getElementById('filmPopUp');
+popUpNode.close();
+
 let filmInfoNode = [];
 
 btnNode.addEventListener('click', function(){
     getFilmFromUser();
-    renderlist();
+
+    searchFilm();
 });
+
 
 
 function getFilmFromUser() {
     if (!inputNode.value) {
-        console.log(`Необходима валидация`);
-        return;
+        inputNode.placeholder='Необходимо ввести название фильма';
+        return
     }
     film = inputNode.value;
     clearInput();
@@ -23,13 +27,30 @@ function getFilmFromUser() {
 
 function clearInput() {
     inputNode.value = '';
+    inputNode.placeholder='Название фильма';
 };
 
-async function renderlist () {
+async function searchFilm () {
+    if (!film) {
+        return
+    }
     const res = await fetch(`http://www.omdbapi.com/?s=${film}&apikey=2e380f8c`);
     const listJson = await res.json();
     list = listJson.Search;
+    if (!list) {
+        listNode.innerHTML = ``;
+        listNode.innerText = `По вашему запросу ничего не найдено`;
+        return
+    }
     
+    renderlist();
+
+    filmInfoNode = document.querySelectorAll('.listOfFilms_resultStr');
+    
+    searchIMDB();
+};
+
+function renderlist() {
     let ListHTML = '';
     list.forEach(element => {
         ListHTML += 
@@ -43,25 +64,40 @@ async function renderlist () {
         </li>`;
     });
     listNode.innerHTML = `<ol>${ListHTML}</ol>`;
+};
 
-
-
-    filmInfoNode = document.querySelectorAll('.listOfFilms_resultStr');
-    
+function searchIMDB () {
     filmInfoNode.forEach((item, index) => {
-        item.addEventListener('click', function(){
+        item.addEventListener('click', async function(){
+            const resIMDB = await fetch(`http://www.omdbapi.com/?i=${list[index].imdbID}&apikey=2e380f8c`);
+            const listIMDBJson = await resIMDB.json();
+            
+            
             popUpNode.innerHTML = `
-            <p class="listofFilms_popUp_title">${list[index].Title}</p>
-            <p class="listofFilms_popUp_chars">Год: ${list[index].Year}</p>
-            <p class="listofFilms_popUp_chars">Рейтинг: ${list[index].Year}</p>
-            `;
-            console.log(list[index]);
+            <button class="listofFilms_popUp_closeButton" onclick="window.filmPopUp.close()">
+                Назад к поиску
+            </button>
+            <div class="listofFilms_popUp_row">
+                <div>
+                    <img src="${listIMDBJson.Poster}" alt="" class="listOfFilms_popUp_poster">
+                </div>
+                <div>
+                    <p class="listofFilms_popUp_title">${listIMDBJson.Title}</p>
+                    <p class="listofFilms_popUp_chars">Год: ${listIMDBJson.Year}</p>
+                    <p class="listofFilms_popUp_chars">Рейтинг: ${listIMDBJson.Rated}</p>
+                    <p class="listofFilms_popUp_chars">Дата выхода: ${listIMDBJson.Released}</p>
+                    <p class="listofFilms_popUp_chars">Длительность: ${listIMDBJson.Runtime}</p>
+                    <p class="listofFilms_popUp_chars">Жанр: ${listIMDBJson.Genre}</p>
+                    <p class="listofFilms_popUp_chars">Режиссер: ${listIMDBJson.Director}</p>
+                    <p class="listofFilms_popUp_chars">Сценарий: ${listIMDBJson.Writer}</p>
+                    <p class="listofFilms_popUp_chars">Актеры: ${listIMDBJson.Actors}</p>
+                </div>
+            </div>`;
 
             window.filmPopUp.showModal();
-
+            
         });
     });
-
 };
 
 
